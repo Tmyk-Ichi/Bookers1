1cf2cce6154d2f7e53014befc2253eb6fca572b9
Tmyk-Ichi <tmyk.ichikawa@gmail.com>
2019-10-16 18:22:35 +0900

    [Finish] 4-8 課題:アプリケーションを作成してみよう

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
+  root 'books#top'
+  get "books" =>"books#index"
+  post "books/create" => "books#create"
+end
